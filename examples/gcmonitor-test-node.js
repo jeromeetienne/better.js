9cb533f0449a8737abb18d27f276b2e4565921fd
Jerome Etienne <jerome.etienne@gmail.com>
2012-10-07 16:47:38 +0200

    implemented gc burnrate - fix #19

--- examples/gcmonitor-test-node.js
+++ examples/gcmonitor-test-node.js
@@ -5,10 +5,10 @@ var GcMonitor	= require('../src/gcmonitor.js');
 new GcMonitor().start();
 
 setInterval(function(){
-	for(var j = 0; j < 1000; j++){
+	for(var j = 0; j < 10000; j++){
 		arr	= new Array(10000)
 		for(var i = 0; i < arr.length; i++){
 			arr[i]	= i;
 		}			
 	}
-}, 300)
+}, 1000/100)
--- src/gcmonitor.js
+++ src/gcmonitor.js
@@ -10,7 +10,6 @@ var GcMonitor	= function(){
 	// init a timer
 	var _this	= this;
 	var timerid	= null;
-	var lastUsedHeap= 0;  // remember the heap size
 	// define function to return used heap size
 	var inNode	= typeof(window) === 'undefined' ? true : false;
 	var usedHeapSize= inNode ? function(){
@@ -37,7 +36,7 @@ var GcMonitor	= function(){
 				var i 		= Math.floor(Math.log(bytes) / Math.log(1024));
 				return Math.round(bytes*precision / Math.pow(1024, i))/precision + ' ' + sizes[i];
 			};
-			console.warn(new Date + " -- GC occured!!! saved", bytesToSize(delta))
+			console.warn(new Date + " -- GC occured!!! saved", bytesToSize(delta), ' consuming at ', bytesToSize(burnrate))
 		}
 		timerid	= setInterval(function(){
 			_this.check(onChange);
@@ -50,6 +49,9 @@ var GcMonitor	= function(){
 		cancelInterval(timerid);	
 	};
 	
+	var lastUsedHeap	= null;
+	var lastTimestamp	= null;
+	var burnrate		= null;	
 	/**
 	 * Check if currently used memory is less than previous check. If so it 
 	 * is assume a GC occured
@@ -59,11 +61,42 @@ var GcMonitor	= function(){
 	this.check	= function(onChange){
 		// parameter polymorphism
 		onChange	= onChange || function(property){}
+
+		if( lastUsedHeap === null ){
+			lastUsedHeap	= usedHeapSize();
+			lastTimestamp	= Date.now();
+			return;
+		}
+
+		var present	= Date.now();
+		var currUsedSize= usedHeapSize();
+
 		// check if the heap size is this cycle is LESS than what we had last
 		// cycle; if so, then the garbage collector has kicked in
-		var delta	= usedHeapSize() - lastUsedHeap;
-		if( delta < 0 )	onChange(-delta);		
-		lastUsedHeap	= usedHeapSize();
+		var deltaMem	= currUsedSize - lastUsedHeap;
+		if( deltaMem < 0 ){
+			onChange(-deltaMem, burnrate);		
+		}else{
+			var deltaTime	= present - lastTimestamp
+			var newBurnrate	= deltaMem / (deltaTime/1000);
+			// if there is a previous burnrate, smooth with it
+			if( burnrate !== null ){
+				burnrate	= burnrate * 0.7 + newBurnrate * 0.3;
+			}else{
+				burnrate	= newBurnrate;
+			}
+		}
+		
+		lastUsedHeap	= currUsedSize;
+		lastTimestamp	= Date.now();
+	}
+	
+	/**
+	 * getter for the burnrate
+	 * @type {Number}
+	 */
+	this.burnrate	= function(){
+		return burnrate;
 	}
 }
 
