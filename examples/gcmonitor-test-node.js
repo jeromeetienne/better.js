a9191b5f85898a3ae17b0ea6d435e754a3c27bd1
Jerome Etienne <jerome.etienne@gmail.com>
2012-10-07 15:18:12 +0200

    more work

---
+++ examples/gcmonitor-test-node.js
@@ -0,0 +1,14 @@
+#!/usr/bin/env node
+
+var GcMonitor	= require('../src/gcmonitor.js');
+
+new GcMonitor().start();
+
+setInterval(function(){
+	for(var j = 0; j < 1000; j++){
+		arr	= new Array(10000)
+		for(var i = 0; i < arr.length; i++){
+			arr[i]	= i;
+		}			
+	}
+}, 300)
