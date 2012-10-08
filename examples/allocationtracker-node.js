da302f4df5d0796169e0209bbc6ade52423fa5e7
Jerome Etienne <jerome.etienne@gmail.com>
2012-10-08 16:42:26 +0200

    initial version of allocationtracker.js

---
+++ examples/allocationtracker-node.js
@@ -0,0 +1,23 @@
+#!/usr/bin/env node
+
+var allocationTracker	= require('../src/allocationtracker.js');
+
+
+
+function aClass(){
+	allocationTracker.record('aClass')
+};
+
+function aClass2(){
+	allocationTracker.record('aClass2')
+};
+
+new aClass(); new aClass();
+new aClass();
+new aClass(); new aClass(); new aClass(); new aClass();
+new aClass2();
+new aClass2();
+
+
+console.dir(allocationTracker._klasses);
+console.log(allocationTracker.reportString());
