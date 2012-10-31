14d6450277a13f5b35748c45ac29c691bd36e6d6
Jerome Etienne <jerome.etienne@gmail.com>
2012-10-31 07:37:22 +0100

    added test on globaldetector

---
+++ tests/test-globaldetector.js
@@ -0,0 +1,25 @@
+var GlobalDetector	= GlobalDetector	|| require('../src/globaldetector.js');
+
+describe('GlobalDetector', function(){
+	
+	var gDetector	= new GlobalDetector();
+	var _global	= typeof(window) === 'undefined' ? global : window;
+	
+	it('should not detect anything when no new global are defined', function(){
+		console.assert( gDetector.check() === false );
+	});
+	
+	it('should detect new global', function(){
+		_global.bla0	= true;
+		console.assert( gDetector.check() === true );
+		delete _global.bla0;
+	});
+
+	it('should detect new global with proper name', function(){
+		_global.bla1	= true;
+		gDetector.check(function(property){
+			console.assert(property === 'bla1');
+		});
+		delete _global.bla1;
+	});
+});
