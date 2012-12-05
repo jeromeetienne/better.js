d2ebd64a157a0b74a5014c431e867e3a6773fdd7
Jerome Etienne <jerome.etienne@gmail.com>
2012-12-05 16:10:02 +0100

    more work

--- examples/helpers/globaldetector-trackusage.js
+++ examples/helpers/globaldetector-trackusage.js
@@ -1,48 +1,43 @@
 /**
- * dump trackUsage() from globaldetector.js
+ * dump trackUsage() from globaldetector.js.
+ * 
+ * It doesn't use 
+ * 
  * @return {String} the list of js commands to track global usage
  */
-GlobalDetector.prototype.dumpTrackUsage	= function(){
-	// for each foundGlobals	
+GlobalDetector.prototype.codeForUsageTracker	= function(){
+	// for each foundGlobals
 	var foundGlobals	= this.foundGlobals();
 	// build the output
-	var output	 = '/* Here, include PropertyAttr.js and FunctionAttr.js */\n';
+	var output	 = '/* Include PropertyAttr.js before. */\n';
 	output		+= '/* PropertyAttr.usageTracker.dump() to dump usage records of all tracked properties */\n';
-	output		+= '/* FunctionAttr.usageTracker.dump() to dump usage records of all tracked functions */\n';
 	// add tracking code for each foundGlobals
 	Object.keys(foundGlobals).forEach(function(globalName){
 		// take the namespace for global
-		var inBrowser 	= typeof(window) !== 'undefined'	? true : false
+		var inBrowser 	= typeof(window) !== 'undefined' ? true : false
 		var _global	= inBrowser ?  window :  global;
 		var globalStr	= inBrowser ? 'window': 'global';		
 		// use functionattr.js if it is a function, propertyattr.js otherwise
-		if( typeof(_global[globalName]) === 'function'){
-			var cmd	 = globalStr+'.'+globalName+' = ';
-			cmd	+= 'FunctionAttr.define('+globalStr+', \''+globalName+'\')';		
-			cmd	+= '.trackUsage().done();'
-			output	+= cmd+'\n';
-		}else{
-			var cmd	 = 'PropertyAttr.define( '+globalStr+', \''+globalName+'\').trackUsage();';		
-			output	+= cmd+'\n';
-		}
+		var cmd	 = 'PropertyAttr.define( '+globalStr+', \''+globalName+'\').trackUsage();';		
+		output	+= cmd+'\n';
 	})
 	// return the output
 	return output;
 };
 
 /**
- * display .dumpTrackUsage() in a new popup window
+ * display .codeForUsageTracker() in a new popup window
  */
-GlobalDetector.prototype.dumpTrackUsageWindow	= function(){
-	var codeStr	= this.dumpTrackUsage();
+GlobalDetector.prototype.codeForUsageTrackerWindow	= function(){
+	var codeStr	= this.codeForUsageTracker();
 	var url		= 'data:text/plain,' + codeStr;
 	window.open(url);
 };
 
 /**
- * display .dumpTrackUsage() in the javascript console
+ * display .codeForUsageTracker() in the javascript console
  */
-GlobalDetector.prototype.dumpTrackUsageConsole	= function(){
-	var codeStr	= this.dumpTrackUsage();
+GlobalDetector.prototype.codeForUsageTrackerConsole	= function(){
+	var codeStr	= this.codeForUsageTracker();
 	console.log(codeStr)
 };
