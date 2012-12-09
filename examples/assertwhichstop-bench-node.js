3c8d113b98a16de2f1265af0cd584afc7debce1b
Jerome Etienne <jerome.etienne@gmail.com>
2012-12-09 12:34:20 +0100

    better bench for assert

--- examples/assertwhichstop-bench-node.js
+++ examples/assertwhichstop-bench-node.js
@@ -1,14 +1,50 @@
 #!/usr/bin/env node
 
-//var assertWhichStop	= require('../src/assertwhichstop.js');
+var assertWhichStop	= assertWhichStop	|| require('../src/assertwhichstop.js');
 
-// overload console.assert
-//assertWhichStop.overloadConsole();
+var badValue	= 2000000;
+var debug	= false;
+var nItem	= 1000000;
 
-
-console.time('10000asserts')
-for(var i = 0; i < 100000; i++){
-	console.assert( i !== 200000 );
+console.time('empty loop - '+nItem+' times')
+for(var i = 0; i < nItem; i++){
 }
-console.timeEnd('10000asserts')
+console.timeEnd('empty loop - '+nItem+' times');
 
+console.time('console.assert()')
+for(var i = 0; i < nItem; i++){
+	console.assert( i !== badValue );
+}
+console.timeEnd('console.assert()');
+
+console.time('debug && console.assert()')
+for(var i = 0; i < nItem; i++){
+	debug && console.assert( i !== badValue );
+}
+console.timeEnd('debug && console.assert()');
+
+console.time('assertWhichStop')
+for(var i = 0; i < nItem; i++){
+	assertWhichStop( i !== badValue );
+}
+console.timeEnd('assertWhichStop');
+
+console.time('debug && assertWhichStop()')
+for(var i = 0; i < nItem; i++){
+	debug && assertWhichStop( i !== badValue );
+}
+console.timeEnd('debug && assertWhichStop()');
+
+assertWhichStop.overloadConsole();
+
+console.time('assertWhichStop acting as console.assert()')
+for(var i = 0; i < nItem; i++){
+	console.assert( i !== badValue );
+}
+console.timeEnd('assertWhichStop acting as console.assert()');
+
+console.time('debug && assertWhichStop() acting as console.assert()')
+for(var i = 0; i < nItem; i++){
+	debug && assertWhichStop( i !== badValue );
+}
+console.timeEnd('debug && assertWhichStop() acting as console.assert()');
