c848470583681282bcd22a8dd9e12dff4ce81c4b
Jerome Etienne <jerome.etienne@gmail.com>
2014-01-24 10:47:53 +0100

    more work

--- examples/betterjs-class.js
+++ examples/betterjs-class.js
@@ -28,31 +28,3 @@ Cat	= Bjs.Class(Cat, {
 })
 
 
-
-//////////////////////////////////////////////////////////////////////////////////
-//		comment								//
-//////////////////////////////////////////////////////////////////////////////////
-
-Cat	= ClassAttr(Cat, {
-	arguments	: [String],
-	privatize	: true,
-	properties	: {
-		name	: String,		
-	},
-	onAfter		: function(instance, args){
-		console.log('onAfter')
-	}
-})
-
-var cat	= new Cat('kitty')
-
-console.assert(cat instanceof Cat)
-console.assert(cat instanceof Animal)
-console.assert(typeof(cat.name) === 'string')
-
-// cat.name	= 99
-console.log('name', cat.name)
-
-// cat.prout	= 99
-
-console.log('salute', cat.salute())
